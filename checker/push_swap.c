3cff0c1b02a7dc0a2ceb2632b0768c88f3e4f250
mdaunois <mathieudaunois@gmail.com>
2018-02-05 14:06:41 +0100

    debut du trie pour grand nombre, optimiser le pivot pour faire mieux

--- checker/push_swap.c
+++ checker/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mdaunois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 15:09:40 by mdaunois          #+#    #+#             */
-/*   Updated: 2018/02/02 17:40:19 by mdaunois         ###   ########.fr       */
+/*   Updated: 2018/02/05 14:05:15 by mdaunois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,28 +44,12 @@ int		check_pile(t_pile pile)
 	return (1);
 }
 
-int		main(int argc, char *argv[])
+void	petite_pile(t_pile pile1, t_pile pile2)
 {
-	t_pile	pile1;
-	t_pile	pile2;
-	char	**mouv;
-	int i;
-
-	i = 0;
-	mouv = malloc(sizeof(char*) * 10);
-	mouv[10] = ft_strdup("stop");
-	if (argc == 0)
-		return (0);
-	if (init_pile(&pile1, &pile2, argc, argv) != 1)
-	{
-		ft_putstr_fd("Error\n", 2);
-		return (0);
-	}
 	while (check(pile1, pile2) == 0)
 	{
 		if (pile1.tab[0] > pile1.tab[pile1.size - 1])
 		{
-		//	ft_putnbr(pile1.tab[pile1.size - 1]);
 			ft_putstr("ra\n");
 			pile1 = ra(pile1);
 		}
@@ -73,60 +57,145 @@ int		main(int argc, char *argv[])
 		{
 			ft_putstr("sa\n");
 			pile1 = sa(pile1);
-			//	mouv[i] = ft_strdup("sa\n");
 		}
 		else if (pile2.size > 1 && pile2.tab[0] < pile2.tab[pile2.size - 1])
 		{
 			ft_putstr("rb\n");
 			pile2 = ra(pile2);
-			//	mouv[i] = ft_strdup("sb\n");
 		}
 		else if (pile2.size > 1 && pile2.tab[0] < pile2.tab[1])
 		{
 			ft_putstr("sb\n");
 			pile2 = sa(pile2);
-			//	mouv[i] = ft_strdup("sb\n");
 		}
 		else if (check_pile(pile1) == 1 && pile2.tab[0] < pile1.tab[0])
 		{
 			ft_putstr("pa\n");
 			pa(&pile2, &pile1);	
 		}
-	/*	else if (pile1.tab[0] > pile2.tab[pile2.size])
-		{
-			ft_putnbr(pile2.tab[pile2.size]);
-			ft_putstr("pb\n");
-			pa(&pile1, &pile2);
-			ft_putstr("rb\n");
-			pile2 = ra(pile2);
-		}
-	*/	else if ((pile1.tab[0] < pile1.tab[1]) && (pile2.size == 0 || pile1.tab[0] >pile2.tab[0]))
+		else if ((pile1.tab[0] < pile1.tab[1]) && (pile2.size == 0 || pile1.tab[0] >pile2.tab[0]))
 		{
 			ft_putstr("pb\n");
 			pa(&pile1, &pile2);
-			//	mouv[i] = ft_strdup("pb\n");
 		}
 		else if (pile1.tab[0] > pile1.tab[pile1.size - 1])
 		{
 			ft_putstr("ra\n");
 			pile1 = ra(pile1);
-			//	mouv[i] = ft_strdup("ra\n");
 		}
 		else if (pile1.size > 1)
 		{
 			ft_putstr("pb\n");
 			pa(&pile1, &pile2);
 		}
+		/*		ft_putstr("pile A =");
+				affiche_tab(pile1);
+				ft_putstr("pile B =");
+				affiche_tab(pile2);
+				*/		//sleep(1);
+	}
+}
+
+int		edit_pivot(t_pile pile)
+{
+	int pivot;
+	int i;
+
+	i = 0;
+	while (i < pile.size)
+	{
+		pivot = (pivot + pile.tab[i]) / 2;
+		i++;
+	}
+	return pivot;
+}
+
+int		isFini(t_pile pile, int pivot)
+{
+	int i;
+
+	i = 0;
+	while (i < pile.size)
+	{
+		if (pile.tab[i] < pivot)
+			return (0);
+	}
+	return (1);
+}
+void	quickSort(t_pile pile1, t_pile pile2)
+{
+	int pivot;
+	int i;
+
+	pivot = pile1.tab[pile1.size - 1];
+/*	ft_putstr("meileur pivot = ");
+	ft_putnbr(edit_pivot(pile1));
+	ft_putstr("\n");
+	ft_putstr("mon pivot = ");
+	ft_putnbr(pivot);
+	ft_putstr("\n");
+	sleep(1);*/
+//	ft_putstr("pile A =");
+//	affiche_tab(pile1);
+//	pivot = edit_pivot(pile1);
+	i = 0;
+	if (pile1.tab[i] == pivot)
+	{
+		petite_pile(pile1, pile2);
+		return;
+	}
+/*	ft_putstr("pivot = ");
+	ft_putnbr(pivot);
+	ft_putstr("\n");
+*/	while (pile1.tab[i] != pivot)
+	{
+		if (pile1.tab[i] < pivot)
+		{
+			ft_putstr("pb\n");
+			pa(&pile1, &pile2);	
+		}
+		else
+		{
+		ft_putstr("ra\n");
+		pile1 = ra(pile1);
+		}
+	/*	ft_putstr("pile A =");
+		affiche_tab(pile1);
+		ft_putstr("pile B =");
+		affiche_tab(pile2);
+*///		sleep(1);
+	}
+//	ft_putstr("------------------------------");
+	//	if(gauche < droite)
+	//		echanger(tableau, gauche, droite);
+	//	else break;
+//	quickSort(pile2, pile1);
+	quickSort(pile1, pile2);
+}
+
+
+int		main(int argc, char *argv[])
+{
+	t_pile	pile1;
+	t_pile	pile2;
+	int i;
+
+	i = 0;
+	if (argc == 0)
+		return (0);
+	if (init_pile(&pile1, &pile2, argc, argv) != 1)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return (0);
+	}
+//	if (pile1.size < 12)
+		petite_pile(pile1, pile2);
+//	else
+//		quickSort(pile1, pile2);
 /*		ft_putstr("pile A =");
 		affiche_tab(pile1);
 		ft_putstr("pile B =");
 		affiche_tab(pile2);
-*/		//sleep(1);
-	}
-/*	ft_putstr("pile A =");
-	affiche_tab(pile1);
-	ft_putstr("pile B =");
-	affiche_tab(pile2);
-*/	return (0);
+*/		return (0);
 
 }
